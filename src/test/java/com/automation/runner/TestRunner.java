b93124e69730241190ee42ecdef9104e5a14e049
Shane Biju <shanegarritsen@gmail.com>
2024-11-07 09:20:45 +0530

    configure allure report

--- src/test/java/com/automation/runner/TestRunner.java
+++ src/test/java/com/automation/runner/TestRunner.java
@@ -7,7 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = "com.automation.steps"
+        glue = "com.automation.steps",
+        plugin = {"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
 )
 public class TestRunner {
 }
